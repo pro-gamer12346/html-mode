dcf2252d61fc91d67f50c866f015c9e1cdb47bcb
pro-gamer12346 <4ri4v4t4ni@gmail.com>
2025-07-16 18:16:42 +0330

    Add files via upload

---
+++ import turtle.py
@@ -0,0 +1,5 @@
+import turtle 
+turtle.Screen().bgcolor("orange")
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.title(" WELCOME TO THE UNDERGROUND YOU WILL NOT LIKE THIS BUT THE USSR JUST REFORMED RAHHHHHHHHHHH")
