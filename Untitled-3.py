f971095ab0d3b02f2461cbcfd4a7919c2ab62932
pro-gamer12346 <4ri4v4t4ni@gmail.com>
2025-07-21 17:42:23 +0330

    Add files via upload

---
+++ Untitled-3.py
@@ -0,0 +1,10 @@
+number = int(input("enter"))
+print("number to be checked :", number)
+if number>0:
+    print("this is positive number")
+elif number<0:
+  print  ("ts num so negative")
+
+else:
+   ("ts num is nothing berguar")
+   
