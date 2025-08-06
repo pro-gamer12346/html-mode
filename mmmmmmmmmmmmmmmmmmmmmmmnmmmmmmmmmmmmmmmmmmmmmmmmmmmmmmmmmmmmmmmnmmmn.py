328de75d17b45b59f8ddcd158422085802381787
pro-gamer12346 <4ri4v4t4ni@gmail.com>
2025-08-06 18:44:48 +0330

    Add files via upload

---
+++ mmmmmmmmmmmmmmmmmmmmmmmnmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmnmmmn.py
@@ -0,0 +1,14 @@
+my_dict = {}
+my_dict = {1: 'apple', 2: 'ball'}
+my_dict = {'name': 'john', 1: {2, 4, 3}}
+my_dict = {'name': 'jack', 'age': 26 }
+print(my_dict['name'])
+print(my_dict.get('age'))
+my_dict['age'] = 27
+print(my_dict)
+my_dict['address'] = 'downtown'
+print(my_dict)
+my_dict.pop('age')
+print(my_dict)
+print(my_dict)
+print(my_dict)
