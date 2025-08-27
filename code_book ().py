4deed859ed25d2c11281be43e748d2f730b9d991
pro-gamer12346 <4ri4v4t4ni@gmail.com>
2025-08-27 08:24:40 +0330

    Add files via upload

--- code_book ().py
+++ code_book ().py
@@ -4,3 +4,11 @@ def add_contact():
     number=input("enter phone number:")
     code_book[name] = number
     print("contact added successfuly")
+def view_contacts(): 
+    if not code_book:
+                               print("no contacts found./n")
+    else:
+                        print("obtain contacts")
+    for name,number in code_book.items:
+                              print(f"{name}: {number}")
+    print ()
--- file = open('Untitled-1.py
+++ file = open('Untitled-1.py
@@ -1,3 +1,8 @@
 file = open('Untitled-1.txt')
 print(file.read())
 file.close()
+
+file_write = open ('codingal.txt')
+file_write.write("filein write mode....")
+file_write.write("hi! i am penguin. i am 1yr. old!!!!!!!!!!!!!!!!!!!")
+file.write.close()
