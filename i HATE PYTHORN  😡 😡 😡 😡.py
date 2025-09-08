a5a2548a66a7f4590a57d34c132b7a7b540c406e
pro-gamer12346 <4ri4v4t4ni@gmail.com>
2025-09-08 17:49:39 +0330

    Add files via upload

---
+++ i HATE PYTHORN  😡 😡 😡 😡.py
@@ -0,0 +1,7 @@
+import random
+def play_game (): 
+    name = input("Hella")
+    secret-number = random.randint(1,100)
+    attempts=0
+    print(f"i am thunking of number in the 1/100 range of death and abysss hint: i just some hint: there are none")
+    while True;
